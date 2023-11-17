454ef7ed8d772b4a60416469b96683fd6377c1a7
zmgmarina <113174444+zmgmarina@users.noreply.github.com>
2023-11-17 15:24:17 +0500

    Update SomeNumberTest.java

--- HW_3/SomeNumberTest.java
+++ HW_3/SomeNumberTest.java
@@ -1,1 +1,41 @@
+package hw3;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import tasks.hw3.SomeNumber;
+import static org.assertj.core.api.Assertions.*;
+
+
+public class SomeNumberTest {
+    private SomeNumber someNumber;
+
+    @BeforeEach
+    void setup(){
+        someNumber = new SomeNumber();
+    }
+    @ParameterizedTest
+    @ValueSource(ints = {6,4,2})
+    void evenNumberTest(int n){
+        assertThat(someNumber.evenOddNumber(n)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {15,3,1})
+    void oddNumberTest(int n){
+        assertThat(someNumber.evenOddNumber(n)).isFalse();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {28,50,99})
+    void numberInInterval(int n){
+        assertThat(someNumber.numberInInterval(n)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {25,3,100})
+    void numberOutOfInterval(int n){
+        assertThat(someNumber.numberInInterval(n)).isFalse();
+    }
+
+}
