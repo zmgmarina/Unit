1add8b0c511db2def960cb53dd25ac98af94d640
zmgmarina <113174444+zmgmarina@users.noreply.github.com>
2023-11-17 15:23:26 +0500

    Update SomeNumber.java

--- HW_3/SomeNumber.java
+++ HW_3/SomeNumber.java
@@ -1,1 +1,13 @@
+package tasks.hw3;
 
+public class SomeNumber {
+    public boolean evenOddNumber(int n) {
+          return n % 2 == 0;
+    }
+
+    public boolean numberInInterval(int n) {
+        return n > 25 && n < 100;
+    }
+
+
+}
