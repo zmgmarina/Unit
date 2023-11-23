fd8f136d7cc0e63d31488f9fa314c9a9ee942f7a
zmgmarina <113174444+zmgmarina@users.noreply.github.com>
2023-11-23 13:14:18 +0500

    Update Controller.java

--- hw_6/controller/Controller.java
+++ hw_6/controller/Controller.java
@@ -1,1 +1,28 @@
+package tasks.hw_6.controller;
+
+import tasks.hw_6.model.ActivityOfList;
+
+public class Controller {
+        ActivityOfList activityOfList;
+
+        public Controller(){
+            this.activityOfList = new ActivityOfList();
+        }
+
+        public int[] createList() {
+            return activityOfList.createList();
+        }
+
+
+        public double averageList(int[] array) {
+            return activityOfList.averageList(array);
+        }
+
+        public void compareList(double resultAverageOne, double resultAverageTwo){
+           activityOfList.compareList(resultAverageOne, resultAverageTwo);
+        }
+
+
+
+}
 
